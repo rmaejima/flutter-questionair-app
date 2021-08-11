5ae2818dea168c70a9978d5c966fa51cfb7c6dea
rmaejima <ryo.maejii@gmail.com>
2021-08-11 19:03:40 +0900

    Delete comments

--- lib/views/components/common/q_button.dart
+++ lib/views/components/common/q_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:questionair_app/providers/questionair_provider.dart';
-import 'package:questionair_app/views/screens/q2.dart';
 
 class QButton extends HookWidget {
   QButton({
--- lib/views/screens/q1.dart
+++ lib/views/screens/q1.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+
 import 'package:questionair_app/views/components/common/q_button.dart';
 import 'package:questionair_app/views/components/common/show_questionair_state.dart';
 import 'package:questionair_app/views/screens/q2.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import '../../providers/questionair_provider.dart';
 
 class Q1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // // コントローラの宣言
-    // final _questionairController = useProvider(questionairProvider.notifier);
-    // // ステートの宣言
-    // final _questionairState =
-    //     useProvider(questionairProvider.select((value) => value));
-
     return Scaffold(
       appBar: AppBar(
         title: Text('あなたの体調は？'),
@@ -39,55 +30,6 @@ class Q1 extends StatelessWidget {
             route: Q2(),
           ),
           ShowQuestionairState(),
-          // ElevatedButton(
-          //   child: const Text('良い'),
-          //   style: ElevatedButton.styleFrom(
-          //     primary: Colors.red,
-          //     onPrimary: Colors.white,
-          //     shape: RoundedRectangleBorder(
-          //       borderRadius: BorderRadius.circular(10),
-          //     ),
-          //   ),
-          //   onPressed: () {
-          //     _questionairController.setParam('良い');
-          //     // エラーが起こる
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute<void>(
-          //         builder: (context) {
-          //           return Q2();
-          //         },
-          //       ),
-          //     );
-          //   },
-          // ),
-          // ElevatedButton(
-          //   child: const Text('普通'),
-          //   style: ElevatedButton.styleFrom(
-          //     primary: Colors.yellow,
-          //     onPrimary: Colors.white,
-          //     shape: RoundedRectangleBorder(
-          //       borderRadius: BorderRadius.circular(10),
-          //     ),
-          //   ),
-          //   onPressed: () {
-          //     _questionairController.setParam('普通');
-          //   },
-          // ),
-          // ElevatedButton(
-          //   child: const Text('悪い'),
-          //   style: ElevatedButton.styleFrom(
-          //     primary: Colors.blue,
-          //     onPrimary: Colors.white,
-          //     shape: RoundedRectangleBorder(
-          //       borderRadius: BorderRadius.circular(10),
-          //     ),
-          //   ),
-          //   onPressed: () {
-          //     _questionairController.setParam('悪い');
-          //   },
-          // ),
-          // Text('${_questionairState.param}'),
         ],
       ),
     );
